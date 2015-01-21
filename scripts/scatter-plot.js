d1ad67f5a28a0cbad803c1a9746f0d0479bd0820
Jordan Papaleo <papaleowebdev@gmail.com>
2015-01-20 22:32:16 -0800

    Added a shim and created average data for each collection

--- scripts/scatter-plot.js
+++ scripts/scatter-plot.js
@@ -1,28 +1,47 @@
+var averagedResults = {};
+
 function getFamousData() {
-    callDb({
-        url: 'https://api.mongolab.com/api/1/databases/perftest/collections/famous?apiKey=vGKkyQqFj2AyYkDjQHNP-zARZaD-7jbl',
-        type: 'GET',
-        dataType: 'json',
-        contentType: "application/json"
-    }).then(function(data) {
-        plotData(data, 'famous');
-    });
+    var dataCollections = ['famous', 'famous-iphone', 'famous-nexus', 'famous-safari', 'famous-chrome'];
+    var allPromises = [];
+
+    for(var i = 0, j = dataCollections.length; i < j; i++) {
+        var collectionName = dataCollections[i];
+        var dbConfig = getDbConfig(collectionName);
+
+        callDb(dbConfig).then(function(data) {
+            mergeResults(data);
+            plotData(data);
+        });
+    }
 }
 
 function getGreensockData() {
-    callDb({
-        url: 'https://api.mongolab.com/api/1/databases/perftest/collections/greensock?apiKey=vGKkyQqFj2AyYkDjQHNP-zARZaD-7jbl',
+    var dataCollections = ['greensock', 'greensock-iphone', 'greensock-nexus', 'greensock-safari', 'greensock-chrome'];
+
+    for(var i = 0, j = dataCollections.length; i < j; i++) {
+        var collection = dataCollections[i];
+
+        callDb(getDbConfig(collection)).then(function(data) {
+            mergeResults(data);
+            plotData(data);
+        });
+    }
+}
+
+function getDbConfig(collectionName) {
+    return {
+        url: 'https://api.mongolab.com/api/1/databases/perftest/collections/' + collectionName + '?apiKey=vGKkyQqFj2AyYkDjQHNP-zARZaD-7jbl',
         type: 'GET',
         dataType: 'json',
-        contentType: "application/json"
-    }).then(function(data) {
-        plotData(data, 'greensock');
-    });
+        contentType: "application/json",
+        collectionName: collectionName
+    };
 }
 
 function callDb(ajaxObj) {
     return $.ajax(ajaxObj)
         .done(function (data) {
+            data.collectionName = ajaxObj.collectionName;
             return data;
         })
         .error(function (XMLHttpRequest, textStatus, errorThrown) {
@@ -30,8 +49,58 @@ function callDb(ajaxObj) {
         });
 }
 
-function plotData(data, className) {
+function mergeResults(data) {
+    var clonedData = Object.create(data);
+    var collection = clonedData.collectionName;
+
+    if(!averagedResults.hasOwnProperty(collection)) {
+        averagedResults[collection] = [];
+    }
+
+    for(var i = 0, j = clonedData.length; i < j; i++) {
+        var records = clonedData[i].records;
+
+        for(var k = 0, l = records.length; k < l; k++) {
+            if(!averagedResults[collection][k]) {
+                averagedResults[collection].push([records[k]]);
+            } else {
+                averagedResults[collection][k].push(records[k])
+            }
+        }
+    }
+
+    averageResults();
+}
+
+function averageResults() {
+    for(var collection in averagedResults) {
+        for(var i = 0, j = averagedResults[collection].length; i < j; i++) {
+            //if the array has already been reduced skip
+
+            if(typeof averagedResults[collection][i] === 'number') {
+                continue;
+            }
+
+            averagedResults[collection][i] = _averageArray(averagedResults[collection][i]);
+        }
+
+        var mutantData = mutateData(averagedResults[collection]);
+        scatterPlotRange(mutantData, collection, true);
+    }
+}
+
+function _averageArray(array) {
+    var totalRecords = array.length;
+    var sum = array.reduce(function(total, num) {
+        return total + num;
+    }, 0);
+    return Math.round((sum / totalRecords) * 100) / 100;
+}
+
+function plotData(data) {
     var chartable = [];
+    var collectionName = data.collectionName;
+    delete data.collectionName;
 
     for(var i = 0, j = data.length; i < j; i++) {
         var records = mutateData(data[i].records);
@@ -41,7 +110,7 @@ function plotData(data, className) {
         }
     }
 
-    plotRange(chartable, className);
+    scatterPlotRange(chartable, collectionName);
 }
 
 //creates a data set of value over time
@@ -55,12 +124,16 @@ function mutateData(data) {
     return records;
 }
 
-function plotRange(data, className) {
+function scatterPlotRange(data, collectionName, isLine) {
+    console.info('collectionName',collectionName);
+    var type = (collectionName.indexOf('famous') != -1) ? 'famous' : 'greensock';
+    var interface = collectionName.replace(type + '-', '');
+
     var margin = {top: 20, right: 15, bottom: 20, left: 40};
     var width = 800 - (margin.left + margin.right);
     var height = 500 - (margin.top + margin.bottom);
 
-    var svg = d3.select('#charts').select('svg');
+    var svg = d3.select('#' + interface).select('svg');
 
     svg.attr('width', width + margin.right + margin.left)
         .attr('height', height + margin.top + margin.bottom)
@@ -76,7 +149,7 @@ function plotRange(data, className) {
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
         .attr('width', width)
         .attr('height', height)
-        .attr('class', 'main ' + className);
+        .attr('class', 'main ' + type);
 
 
     // draw the x axis
@@ -93,7 +166,6 @@ function plotRange(data, className) {
         .attr('class', 'main axis date')
         .call(xAxis);
     // end draw x axis
-
 
     // draw the y axis
     var y = d3.scale.linear()
@@ -113,12 +185,23 @@ function plotRange(data, className) {
 
     var g = main.append("svg:g");
 
-    g.selectAll("scatter-dots")
-        .data(data)
-        .enter().append("svg:circle")
-        .attr("cx", function (d,i) { return x(d[0]); } )
-        .attr("cy", function (d) { return y(d[1]); } )
-        .attr("r", 2);
+    if(isLine) {
+        var line = d3.svg.line()
+            .x(function (d,i) { return x(d[0]); })
+            .y(function (d) { return y(d[1]); });
+
+        g.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
+    } else {
+        g.selectAll("scatter-dots")
+            .data(data)
+            .enter().append("svg:circle")
+            .attr("cx", function (d,i) { return x(d[0]); } )
+            .attr("cy", function (d) { return y(d[1]); } )
+            .attr("r", 2);
+    }
 }
 
 getFamousData();
